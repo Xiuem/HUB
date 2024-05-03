8580561ee013cfc47ec0ce9fe2e3824699286c03
Xiuem <160484851+Xiuem@users.noreply.github.com>
2024-05-03 18:50:27 +0700

    Add files via upload

--- 07.lua
+++ 07.lua
@@ -3621,7 +3621,10 @@ spawn(function()
     end)
 end)
     
-    Tabs.Main:AddSection("Auto Fram Boss")
+        Tabs.Ms:AddParagraph({
+        Title = "Auto Fram Boss",
+        Content = ""
+    })
     
     if World1 then
 		tableBoss = {"The Gorilla King","Bobby","Yeti","Mob Leader","Vice Admiral","Warden","Chief Warden","Swan","Magma Admiral","Fishman Lord","Wysper","Thunder God","Cyborg","Saber Expert"}
@@ -3631,8 +3634,8 @@ end)
 		tableBoss = {"Stone","Island Empress","Kilo Admiral","Captain Elephant","Beautiful Pirate","rip_indra True Form","Longma","Soul Reaper","Cake Queen"}
 	end
 	
-	local Dropdown = Tabs.Mt:AddDropdown("Dropdown", {
-        Title = "Select Boss Fram",
+	local Dropdown = Tabs.Ms:AddDropdown("Dropdown", {
+        Title = "select Boss Fram",
         Values = tableBoss,
         Multi = false,
         Default = 1,
@@ -3642,4 +3645,41 @@ end)
 
     Dropdown:OnChanged(function(Value)
         _G.SelectBoss = Value
+    end)
+    
+    local Toggle = Tabs.Ms:AddToggle("MyToggle", {Title = "Auto Farm Boss", Default = false })
+
+    Toggle:OnChanged(function(Value)
+        _G.AutoFarmBoss = Value
+		StopTween(_G.AutoFarmBoss)
+    end)
+    
+    spawn(function()
+        while wait() do
+            if _G.AutoFarmBoss then
+                pcall(function()
+                    if game:GetService("Workspace").Enemies:FindFirstChild(_G.SelectBoss) then
+                        for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+                            if v.Name == _G.SelectBoss then
+                                if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
+                                    repeat task.wait()
+                                        AutoHaki()
+                                        EquipWeapon(_G.SelectWeapon)
+                                        v.HumanoidRootPart.CanCollide = false
+                                        v.Humanoid.WalkSpeed = 0
+                                        v.HumanoidRootPart.Size = Vector3.new(80,80,80)                             
+                                        topos(v.HumanoidRootPart.CFrame * CFrame.new(PosX,PosY,PosZ))
+                                        sethiddenproperty(game:GetService("Players").LocalPlayer,"SimulationRadius",math.huge)
+                                    until not _G.AutoFarmBoss or not v.Parent or v.Humanoid.Health <= 0
+                                end
+                            end
+                        end
+                    else
+                        if game:GetService("ReplicatedStorage"):FindFirstChild(_G.SelectBoss) then
+                            topos(game:GetService("ReplicatedStorage"):FindFirstChild(_G.SelectBoss).HumanoidRootPart.CFrame * CFrame.new(5,10,2))
+                        end
+                    end
+                end)
+            end
+        end
     end)
